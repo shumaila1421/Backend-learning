3a132e9992ea1ffad257710b1062cc20be3e2a14
Shumaila Riaz <shumailariaz215@gmail.com>
2026-03-25 19:51:44 +0500

    commits added

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
-const app = express();
+const app = express(); //server created
 
 app.get("/", (req, res) => {
+  //server is programmed to send response
   res.send("Hello World");
 });
 
-app.listen(5000);
+app.listen(5000); //server start
